3d5316682b90cbe972730209eeaf4b04832f0e1d
PerairaCC <carloscoto0103@gmail.com>
2021-04-24 19:55:45 -0600

    Se agrega la funcionalidad de recibir la pregunta y enviar la pregunta

--- Servidor/Program.cs
+++ Servidor/Program.cs
@@ -12,31 +12,31 @@ namespace Servidor
     {
        
         
-            //Jugador jugadorClase;
-            TcpClient cliente1Clase;
-            string numeroCliente = "0";
-            int puntaje = 0;
+        //Jugador jugadorClase;
+        TcpClient cliente1Clase;
+        string numeroCliente = "0";
+        int puntaje = 0;
 
-            static void Main(string[] args)
+        static void Main(string[] args)
+        {
+            TcpListener servidorSocket = new TcpListener(8080);
+            TcpClient cliente = default(TcpClient);
+            //Jugador jugador = new Jugador();
+            int canCliente = 0;
+
+            servidorSocket.Start();
+            Console.WriteLine(" >> " + "Servidor Iniciado");
+
+            while (true)
             {
-                TcpListener servidorSocket = new TcpListener(8080);
-                TcpClient cliente = default(TcpClient);
-                //Jugador jugador = new Jugador();
-                int canCliente = 0;
-
-                servidorSocket.Start();
-                Console.WriteLine(" >> " + "Servidor Iniciado");
-
-                while (true)
-                {
-                    canCliente += 1;
-                    cliente = servidorSocket.AcceptTcpClient();
-                    Console.WriteLine("Cliente: " + canCliente + " Conectado.");
-                    Servidor servidor = new Servidor();
-                    servidor.InicioCliente(cliente,  Convert.ToString(canCliente));
-                }
-
+                canCliente += 1;
+                cliente = servidorSocket.AcceptTcpClient();
+                Console.WriteLine("Cliente: " + canCliente + " Conectado.");
+                Servidor servidor = new Servidor();
+                servidor.InicioCliente(cliente,  Convert.ToString(canCliente));
             }
+
+        }
 
         public void InicioCliente(TcpClient cliente,  string numeroCliente)
         {
@@ -67,17 +67,53 @@ namespace Servidor
 
                 string[] data = mensajeCliente.Split(',');
 
-                Console.WriteLine(data[0]);
-                Console.WriteLine(data[1]);
+                if (data[0].Equals("profesor"))
+                {
+                    if(Preguntas.pregunta == null)
+                    {
+                        Preguntas.pregunta = data[1];
+                        Preguntas.respuesta = data[2];
 
-                Console.ReadLine();
+                        NetworkStream serverStream = cliente1Clase.GetStream();
+                        bytesServidor = System.Text.Encoding.ASCII.GetBytes("Pregunta recibida" + "$");
+                        serverStream.Write(bytesServidor, 0, bytesServidor.Length);
+                        serverStream.Flush();
 
+                        Console.WriteLine("Se recibe la pregunta");
+                    }
+                }
+                else if (data[0].Equals("estudiante"))
+                {
+                    if (Preguntas.pregunta != null)
+                    {
+                        NetworkStream serverStream = cliente1Clase.GetStream();
+                        bytesServidor = System.Text.Encoding.ASCII.GetBytes(Preguntas.pregunta + "$");
+                        serverStream.Write(bytesServidor, 0, bytesServidor.Length);
+                        serverStream.Flush();
 
-            }
-            
+                        Console.WriteLine("Se le envia la pregunta al estudiante");
+                    }
+                    else
+                    {
+                        NetworkStream serverStream = cliente1Clase.GetStream();
+                        bytesServidor = System.Text.Encoding.ASCII.GetBytes("No existe pregunta" + "$");
+                        serverStream.Write(bytesServidor, 0, bytesServidor.Length);
+                        serverStream.Flush();
+
+                        Console.WriteLine("Se le indica al estudiante que no existe pregunta");
+                    }
+                }
+            }    
         }
     }
 
+    public static class Preguntas
+    {
+
+        public static string pregunta { get; set; }
+
+        public static string respuesta { get; set; }
+    }
 
 
 }
